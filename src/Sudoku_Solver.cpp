85269fb57c594ab5bcb1a741cc2cdfdf7419c086
FunnyOwl27 <funnyowl27@proton.me>
2025-06-06 17:52:00 +0530

    Added all checks for a valid input into the sudoku

--- src/Sudoku_Solver.cpp
+++ src/Sudoku_Solver.cpp
@@ -26,3 +26,49 @@ bool Sudoku_Solver::_checkRow(int index)
 	}
 	return true;
 }
+
+bool Sudoku_Solver::_checkColumn(int index)
+{
+	int column = index % 9;
+	unordered_set<int> occurred;
+	for (int i = column; i <= column + 9 * 8; i = i + 9)
+	{
+		if (occurred.find(this->sudoku[i]) != occured.end())
+		{
+			return false;
+		}
+		else
+		{
+			occurred.insert(this->sudoku[i]);
+		}
+	}
+	return true;
+}
+
+bool Sudoku_Solver::_checkSquare(int index);
+{
+	int row = index / 9;
+	int column = index % 9;
+	int sqRow = row - row % 3;
+	int sqColumn = column - column % 3;
+
+	int chIndex;
+
+	unordered_set<int> occurred;
+	for (int i = 0; i < 3; i++)//row
+	{
+		for (int j = 0; j < 3; j++)//column
+		{
+			chIndex = (sqRow + i) * 9 + (sqColumn + j);
+			if (occurred.find(this->sudoku[i]) != occured.end())
+			{
+				return false;
+			}
+			else
+			{
+				occurred.insert(this->sudoku[i]);
+			}
+		}
+	}
+	return true;
+}
