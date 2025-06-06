af03cb161d95069d8c04434e97c3edd1edc67176
FunnyOwl27 <funnyowl27@proton.me>
2025-06-06 17:19:15 +0530

    Created solve class

--- src/Sudoku_Solver.cpp
+++ src/Sudoku_Solver.cpp
@@ -1,3 +1,28 @@
 ﻿
 #include "Sudoku_Solver.h"
+#include <unordered_set>
+using std::unordered_set;
+using std::array;
+Sudoku_Solver::Sudoku_Solver(array<int, 81> arr) : _sudoku(arr)
+{
+	std::cout << "Created" << '\n';
+}
+Sudoku_Solver::~Sudoku_Solver() { std::cout << "Distroyed" }
 
+bool Sudoku_Solver::_checkRow(int index)
+{
+	int row = index / 9;
+	unordered_set<int> occurred;
+	for (int i = row * 9; i < (row + 1) * 9; i++)
+	{
+		if (occurred.find(this->sudoku[i]) != occured.end())
+		{
+			return false;
+		}
+		else
+		{
+			occurred.insert(this->sudoku[i]);
+		}
+	}
+	return true;
+}
--- src/Sudoku_Solver.h
+++ src/Sudoku_Solver.h
@@ -1,1 +1,14 @@
 ﻿#pragma once
+#import <array>
+class Sudoku_Solver
+{
+private:
+	std::array<int, 81> _sudoku;
+	bool _checkRow(int index);
+	bool _checkColumn(int index);
+	bool _checkSquare(int index);
+public:
+	Sudoku_Solver(std::array<int, 81>);
+	~Sudoku_Solver();
+	void solve();
+};
