b0af3b861786190140653928ba95b06d512ffd33
FunnyOwl27 <funnyowl27@proton.me>
2025-06-15 23:51:43 +0530

    Selecting squares using Mouse added

--- src/gui.cpp
+++ src/gui.cpp
@@ -16,25 +16,50 @@ int main()
     for (int i = 0; i < 81; i++)
     {   
         grid[i].setFillColor({25,25,25});
-        //grid[i].setOrigin(grid[i].getSize().componentWiseDiv({2,2}));
-        //std::cout << grid->getOrigin().x;
         grid[i].setPosition({paddingx + grid->getSize().x/2 + i % 9 * (grid->getSize().x + 2) + ((i % 9) / 3) * 3, paddingy + grid->getSize().y/2 + i / 9 * (grid->getSize().x + 2) + ((i / 9) / 3) * 3});
     }
-    // sf::RectangleShape background({8 * (grid[0].size + 5) + grid->size + 10, 8 * (grid[0].size + 5) + grid->size + 10});
+    
     sf::RectangleShape background(grid[80].getPosition() - grid[0].getPosition() + grid->getSize() + sf::Vector2f{10,10});
     background.setOrigin({background.getSize().x / 2, background.getSize().y / 2});
     background.setPosition(grid[4 + 4 * 9].getPosition());
     background.setFillColor({70, 70, 70});
-    //std::cout << SIZE;
+    
     while (window.isOpen())
     {
-        //std::cout <<std::boolalpha <<  window.hasFocus() << '\n';
+        
         // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
             // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
                 window.close();
+            // mouse left button pressed event -  to select and fill numbers in grid
+            if (const auto* pressed = event->getIf<sf::Event::MouseButtonReleased>())
+            {
+                if (pressed->button == sf::Mouse::Button::Left)
+                {
+                    bool Clicked_Grid = false;
+                    for(int i = 0 ; i<81;i++)
+                    {
+                        if(grid[i].getGlobalBounds().contains(static_cast<sf::Vector2f>(pressed->position)))
+                        {
+                            Clicked_Grid = true;
+
+                            if (Sudoku_Square::SELECTED > -1)
+                                grid[Sudoku_Square::SELECTED].setFillColor({25,25,25});
+                            Sudoku_Square::SELECTED = i;
+                            grid[Sudoku_Square::SELECTED].setFillColor({230,230,230});
+                            
+                            break;
+                        }
+                    }
+                    if (!Clicked_Grid)
+                    {
+                        grid[Sudoku_Square::SELECTED].setFillColor({25,25,25});
+                        Sudoku_Square::SELECTED = -1;
+                    }
+                }
+            }
         }
         // DRAW STUFF
         window.clear({20, 20, 20});
